4a6b0416d5ef9c86102358053cede899694ba60c
Eric-Atlantis <eric2001201@163.com>
2021-06-05 17:02:48 +0800

    Update Route.h, Routes.h

--- BasicDataClass/Route.h
+++ BasicDataClass/Route.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "Airport.h"
 
 class Route
@@ -7,43 +8,64 @@ class Route
 private:
 	Airport* TKOF_AP_Ptr;
 	Airport* DEST_AP_Ptr;
-	int duration; // store in minus
+	double duration; // second(s)
 
 public:
 	Route();
-	Route(Airport p_DEP_AP, Airport p_DEST_AP, int p_duration);
+	Route(Airport &p_TKOF_AP, Airport &p_DEST_AP, double p_duration);
 
-	Airport getDEP_AP();
+  void hardcopy(Route p_route);
+
+	Airport getTKOF_AP();
 	Airport getDEST_AP();
-	int getDuration();
+	double getDuration();
 
   bool isSameAPIncluded(Airport p_AP);
+  bool isSameRoute(Route p_route);
 };
-
 
 /* Route class */
 
 Route::Route() {
 	TKOF_AP_Ptr = nullptr;
 	DEST_AP_Ptr = nullptr;
-	duration = 0;
+	duration = -1.0;
 }
-Route::Route(Airport p_DEP_AP, Airport p_DEST_AP, int p_duration) {
-	TKOF_AP_Ptr = &p_DEP_AP;
+Route::Route(Airport &p_TKOF_AP, Airport &p_DEST_AP, double p_duration) {
+	TKOF_AP_Ptr = &p_TKOF_AP;
 	DEST_AP_Ptr = &p_DEST_AP;
 	duration = p_duration;
 }
 
-Airport Route::getDEP_AP() {
+void Route::hardcopy(Route p_route) {
+  this->TKOF_AP_Ptr = p_route.TKOF_AP_Ptr;
+  this->DEST_AP_Ptr = p_route.DEST_AP_Ptr;
+  this->duration = p_route.duration;
+}
+
+Airport Route::getTKOF_AP() {
 	return *TKOF_AP_Ptr;
 }
 Airport Route::getDEST_AP() {
 	return *DEST_AP_Ptr;
 }
-int Route::getDuration() {
+double Route::getDuration() {
 	return duration;
 }
 
 bool Route::isSameAPIncluded(Airport p_AP) {
-	return TKOF_AP_Ptr->isSameAirport(p_AP) || DEST_AP_Ptr->isSameAirport(p_AP);
+	if (TKOF_AP_Ptr->isSameAirport(p_AP)
+      || DEST_AP_Ptr->isSameAirport(p_AP)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+bool Route::isSameRoute(Route p_route) {
+  if (TKOF_AP_Ptr->isSameAirport(*(p_route.TKOF_AP_Ptr))
+      && DEST_AP_Ptr->isSameAirport(*(p_route.DEST_AP_Ptr))) {
+    return true;
+  } else {
+    return false;
+  }
 }
