50e0347ac1540b99814f105f3c0cecbcf46661d3
lilongsky <changtian.li19@student.xjtlu.edu.cn>
2021-06-05 16:56:25 +0800

    modify User and Users base on AP and APs

--- BasicDataClass/User.h
+++ BasicDataClass/User.h
@@ -12,6 +12,11 @@ private:
 public:
   User() {}
   User(std::string userID, std::string realName, std::string role);
+  void hardcopy(User p_User);
+  std::string getUserId();
+  std::string getUserName();
+  std::string getUserRole();
+  bool isSameUser(User p_user);
 };
 
 /* User class */
@@ -20,4 +25,27 @@ User::User(std::string userID, std::string realName, std::string role) {
   m_userID = userID;
   m_realName = realName;
   m_role = role;
+}
+
+void User::hardcopy(User p_User){
+	this->m_realName = p_User.m_realName;
+	this->m_userID = p_User.m_userID;
+	this->m_role = p_User.m_role;
+}
+
+std::string User::getUserId(){
+	return this->m_userID;
+}
+
+std::string User::getUserName(){
+	return this->m_realName;
+}
+std::string User::getUserRole(){
+	return this->m_role;
+}
+
+bool User::isSameUser(User p_user){
+	bool ans;
+	ans = (this->m_userID == p_user.m_userID);
+	return ans;
 }
--- VectorClass/Users.h
+++ VectorClass/Users.h
@@ -13,6 +13,8 @@ public:
 
   void add(User &p_user);
   void remove(int p_index);
+  int getUserIndex(User p_User);
+  bool isUserInUsers(User p_User);
 };
 
 /* Users class */
@@ -23,4 +25,23 @@ void Users::add(User &p_user) {
 
 void Users::remove(int p_index){
 	userVector.erase(userVector.begin() + p_index);
+}
+bool Users::isUserInUsers(User p_User){
+	if (getUserIndex(p_User) == -1){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+int Users::getUserIndex(User p_User){
+	int ans = -1;
+	for (int i = 0; i < userVector.size(); i++){
+		if (userVector.at(i).isSameUser(p_User)){
+			ans = i;
+			break;
+		}
+	}
+	return ans;
 }
