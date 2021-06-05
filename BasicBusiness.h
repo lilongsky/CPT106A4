1faacb84aa4d267eb62702ee174046a2f9fc9729
lilongsky <changtian.li19@student.xjtlu.edu.cn>
2021-06-05 18:23:57 +0800

    modify BasicBusiness Class

--- BasicBusiness.h
+++ BasicBusiness.h
@@ -2,6 +2,9 @@
 #include"FileOp.h"
 #include"DataOp.h"
 class BasicBusiness{
+private:
+	DataOp* dataOpPtr;
+	FileOp* fileOpPtr;
 public:
 	void addNewAirport(std::string p_name);
 	void addNewRote(std::string p_src, std::string p_dest, double p_durition);
@@ -12,8 +15,42 @@ public:
 	void payForTicket(std::string p_TicketId);
 	int getPassagerOnFlight(std::string p_FlightId);
 	int getRevenue(time_t p_StartTime, time_t p_EndTime);
-	int getSellTicketId(std::string p_UserId);
+	int getSellTicketNumbers(std::string p_UserId);
 };
 void BasicBusiness::addNewAirport(std::string p_name){
+	if (true/*No Same Airport */){
+		dataOpPtr->addAirport(p_name);
+		fileOpPtr->updateAirportsFile(dataOpPtr->getAPVectorCopy());
+	}
+	//else{
+	//	throw();
+	//}
+}
+void BasicBusiness::addNewRote(std::string p_src, std::string p_dest, double p_durition){
+	if (true/*No same Route */){
+		dataOpPtr->addRoute(p_src, p_dest, p_durition);
+		/*fileOpPtr->updateRoutesFile(dataOpPtr->getRouteVectorCopy())*/
+	}
+	//else{
+	//	throw()
+	//}
+}
+void BasicBusiness::addNewPlane(std::string p_plane, std::string p_type){
+	if (true/* No Same Plane*/){
+		
+	}
+}
+
+void BasicBusiness::bookTicket(std::string p_UserIdC, std::string p_planeId, std::string p_seat, std::string p_UserIdTA){
+	//confirm their is a seat for the flight
+
+	//get time 
+}
+void BasicBusiness::payForTicket(std::string p_FlightId, std::string p_UserId, std::string p_seat){
 
 }
+
+void BasicBusiness::payForTicket(std::string p_TicketId){
+
+}
+
